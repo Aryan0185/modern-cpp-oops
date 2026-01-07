9f96de06c89570921591761c7420589d4ea4886c
Aryan <aryansaini42817@gmail.com>
2026-01-07 17:20:05 +0530

    It is a diamond pattern.

---
+++ Pattern/pattern-20.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+class pattern{
+    int n;
+    public:
+    void getdata(){
+        cout<<"Enter the number of rows: ";
+        cin>>n;
+    }
+    void display(){
+        //for rows loops 
+        for(int i=1;i<=n;i++){
+            //free space 
+            for(int j=n;j>=i;j--){
+                cout<<"  ";
+            }
+            //print star
+            for(int j=1;j<2*i;j++){
+                cout<<j<<" ";
+            }
+            cout<<endl;
+        }
+       
+        for(int i=1;i<n;i++){
+            //lower half triangle
+            for(int j=1;j<=i+1;j++){
+                cout<<"  ";
+            }
+            //print star
+            for(int j=1;j<2*(n-i);j++){
+                cout<<j<<" ";
+            }
+            //end line
+            cout<<endl;
+        }
+    }
+};
+int main(){
+    pattern p;
+    p.getdata();
+    p.display();
+    return 0;
+}

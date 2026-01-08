47955c492ca2f0cb7d1a99bf2a011dff4178d409
Aryan <aryansaini42817@gmail.com>
2026-01-08 16:52:53 +0530

    It is a Hollow diamond.

---
+++ Pattern/pattern-24.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+class pattern{
+    int n;
+    public:
+    void getdata(){
+        cout<<"Enter the number of rows: ";
+        cin>>n;
+    }
+    void display(){
+        //for rows loops 
+        for(int i=1;i<=n;i++){
+            //free space 
+            for(int j=1;j<=n-i+1;j++){
+                cout<<"* ";
+            }
+            //print star
+            for(int j=1;j<=2*i-1;j++){
+                cout<<"  ";
+            }
+            for(int j=1;j<=n-i+1;j++){
+                cout<<"* ";
+            }
+            cout<<endl;
+        }
+
+        for(int i=n-1;i>=1;i--){
+            //lower half triangle
+            for(int j=n;j>=i;j--){
+                cout<<"* ";
+            }
+            //print star
+            for(int j=1;j<=2*i-1;j++){
+                cout<<"  ";
+            }
+            for(int j=1;j<=n-i+1;j++){
+                cout<<"* ";
+            }
+            //end line
+            cout<<endl;
+        }
+    }
+};
+int main(){
+    pattern p;
+    p.getdata();
+    p.display();
+    return 0;
+}
